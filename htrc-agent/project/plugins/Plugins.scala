4418341d6e71fe72b5373299d4c399e20bc2829c
Aaron Todd <toddaaro@gmail.com>
2011-10-11 01:01:42 +0000

    Changed Akka version to 1.2 from 1.1.2. This is a non-breaking update. All current code will work fine but we can now gradually shift to the new preferred way to deal with futures representing message replies. Our current "!!" approach is deprecated and removed in the current 2.0 build.

--- htrc-agent/project/plugins/Plugins.scala
+++ htrc-agent/project/plugins/Plugins.scala
@@ -2,7 +2,7 @@ import sbt._
  
 class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
   val akkaRepo = "Akka Repo" at "http://akka.io/repository"
-  val akkaPlugin = "se.scalablesolutions.akka" % "akka-sbt-plugin" % "1.1.2"
+  val akkaPlugin = "se.scalablesolutions.akka" % "akka-sbt-plugin" % "1.2"
 
   override def ivyXML =
         <dependencies>
