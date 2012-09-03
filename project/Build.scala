38bec31271d77ac9ef4eddc2d982019bba7011d3
Aaron Todd <toddaaro@gmail.com>
2012-09-03 17:31:55 +0000

    messing with build config

--- project/Build.scala
+++ project/Build.scala
@@ -1,13 +1,20 @@
 
 import sbt._
 import Keys._
+import akka.sbt.AkkaKernelPlugin
+import akka.sbt.AkkaKernelPlugin.{ Dist, outputDirectory, distJvmOptions }
+
 
 object AgentBuild extends Build {
 
   lazy val root = Project(id = "root", base = file(".")) aggregate(rest, client, http)
 
-  lazy val rest = Project(id = "rest", base = file("agent"), 
-                          settings = Project.defaultSettings).settings(mainClass in (Compile, run) := Some("play.core.server.NettyServer")).dependsOn("http")
+  lazy val rest = Project(
+    id = "rest", 
+    base = file("agent"), 
+    settings = Project.defaultSettings ++ AkkaKernelPlugin.distSettings ++ Seq(
+      distJvmOptions in Dist := "-Xms 1024M -Xmx12G",
+      outputDirectory in Dist := file("target/rest-dist"))).settings(mainClass in (Compile, run) := Some("play.core.server.NettyServer")).dependsOn("http")
 
   lazy val worker = Project(id = "worker",
                             base = file("agent")).dependsOn("http").dependsOn("rest")
@@ -17,5 +24,7 @@ object AgentBuild extends Build {
 
   lazy val http = Project(id = "http",
                           base = file("http"))
+
+  
 
 }
