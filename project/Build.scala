6b57a0b0af69fddf9088122ec73544893cc916a8
Aaron Todd <toddaaro@gmail.com>
2012-09-07 17:14:45 +0000

    undid damage to build file

--- project/Build.scala
+++ project/Build.scala
@@ -1,9 +1,6 @@
 
 import sbt._
 import Keys._
-import akka.sbt.AkkaKernelPlugin
-import akka.sbt.AkkaKernelPlugin.{ Dist, outputDirectory, distJvmOptions }
-
 
 object AgentBuild extends Build {
 
@@ -12,9 +9,7 @@ object AgentBuild extends Build {
   lazy val rest = Project(
     id = "rest", 
     base = file("agent"), 
-    settings = Project.defaultSettings ++ AkkaKernelPlugin.distSettings ++ Seq(
-      distJvmOptions in Dist := "-Xms 1024M -Xmx12G",
-      outputDirectory in Dist := file("target/rest-dist"))).settings(mainClass in (Compile, run) := Some("play.core.server.NettyServer")).dependsOn("http")
+    settings = Project.defaultSettings).settings(mainClass in (Compile, run) := Some("play.core.server.NettyServer")).dependsOn("http")
 
   lazy val worker = Project(id = "worker",
                             base = file("agent")).dependsOn("http").dependsOn("rest")
