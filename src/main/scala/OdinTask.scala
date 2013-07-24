4b7c38a7d29c5ef3add46d90d0122fb411d6fb32
Leena Unnikrishnan <unnikrishnan.leena@gmail.com>
2013-07-24 00:31:56 +0000

    suppress stderr output resulting from 'ssh -t -t'

--- src/main/scala/OdinTask.scala
+++ src/main/scala/OdinTask.scala
@@ -133,7 +133,7 @@ class OdinTask(user: HtrcUser, inputs: JobInputs, id: JobId) extends Actor {
 
       // Our "system" parameters can be set as environment variables
       val env = "HTRC_WORKING_DIR=agent_working_directories/%s".format(id)
-      val cmdF = "ssh -t -t %s %s srun -N1 bash ~/agent_working_directories/%s/%s"
+      val cmdF = "ssh -t -t -q %s %s srun -N1 bash ~/agent_working_directories/%s/%s"
       val cmd = cmdF.format(odin, env, id, inputs.runScript)
       
       val sysProcess = SProcess(cmd, new File(workingDir))
