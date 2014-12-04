431f441bdc450f26dda75e0945b5fee762f43ed7
U-pcone\lunnikri <unnikrishnan.leena@gmail.com>
2014-12-03 20:59:38 -0500

    Remove port forwarding on the compute resource.

--- agent-api/src/main/resources/SkeletonAgentJobClient.sh
+++ agent-api/src/main/resources/SkeletonAgentJobClient.sh
@@ -4,13 +4,4 @@
 # the algorithm child process run
 echo "Hostname: `hostname --fqdn`"
 
-# set up port forwarding to the agent
-ssh -f drhtrc@htrc4.pti.indiana.edu -L 19003:htrc4.pti.indiana.edu:9000 -N
-
 $JAVA_CMD -Xmx512m -jar $HOME/leena/agent-job-client/AJC/AgentJobClient.jar -d $HTRC_WORKING_DIR -s $SHELL -t $HTRC_TIME_LIMIT -j $HTRC_JOBID -a $HTRC_AGENT_ENDPOINT $HTRC_WORKING_DIR/$HTRC_ALG_SCRIPT
-
-# remove port forwarding
-SSHTUNNEL=`ps aux | grep "ssh -f drhtrc" | grep -v "grep" | awk '{print $2}'`
-echo "SSH tunnel pid = $SSHTUNNEL"
-kill -9 $SSHTUNNEL
-echo "Result of kill ssh tunnel = $?"
